a1f2496a96d8822eafe70c1bf0b22850f7d0b494
Max Heidinger <mail@max-heidinger.de>
2021-01-16 12:04:22 +0100

    Add permissions and twilio deps

--- app/views/patient/InDoctorAppointment.tsx
+++ app/views/patient/InDoctorAppointment.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, PermissionsAndroid, ToastAndroid} from 'react-native';
 
-const InDoctorAppointment = ({navigation}: {navigation: any}) => {
-  return (
-    <View style={styles.container}>
-      <Text>In Doctor Appointment</Text>
-    </View>
-  );
+const requestPermission = async (): Promise<boolean> => {
+  const granted = await PermissionsAndroid.requestMultiple([
+    PermissionsAndroid.PERMISSIONS.CAMERA,
+    PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+  ]);
+  if (granted[PermissionsAndroid.PERMISSIONS.CAMERA] === PermissionsAndroid.RESULTS.GRANTED &&
+      granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] === PermissionsAndroid.RESULTS.GRANTED) {
+    console.log("Requesting permissions successful")
+    return true;
+  } else {
+    console.log("Requesting permissions failed")
+    return false;
+  }
 };
+
+type InDoctorAppointmentState = {
+  hasPermissions: boolean;
+  navigation: any;
+};
+
+class InDoctorAppointment extends Component<{}, InDoctorAppointmentState> {
+  componentDidMount() {
+    requestPermission().then((gotPermission) => {
+      if (gotPermission) {
+        this.setState({
+          hasPermissions: true
+        });
+      } else {
+        ToastAndroid.show("Permissions required for appointment!", ToastAndroid.LONG);
+        this.state.navigation.goBack();
+      }
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>In Doctor Appointment</Text>
+      </View>
+    );
+  }
+}
 
 export default InDoctorAppointment;
 
